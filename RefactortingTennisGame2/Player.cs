1c97dd0ac0c047ca482cc8cf9fd384c53a63b41f
wenjia.wu <wenjia.wu@thoughtworks.com>
2020-07-31 22:42:20 +0800

    refactor: add Player class.

---
+++ RefactortingTennisGame2/Player.cs
@@ -0,0 +1,14 @@
+namespace RefactortingTennisGame2
+{
+    public class Player
+    {
+        private string Name;
+        public int Point;
+        public string Result;
+        
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- RefactortingTennisGame2/TennisGameImpl.cs
+++ RefactortingTennisGame2/TennisGameImpl.cs
@@ -2,105 +2,100 @@ namespace RefactortingTennisGame2
 {
     public class TennisGameImpl : ITennisGame
     {
-        public int Player1point = 0;
-        public int Player2point = 0;
-
-        public string Player1res = "";
-        public string Player2res = "";
-        private string player1Name;
-        private string player2Name;
+        private Player Player1;
+        public Player Player2;
 
         public TennisGameImpl(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            Player1 = new Player(player1Name);
+            Player2 = new Player(player2Name);
         }
 
         public string GetScore()
         {
             string score = "";
-            if (Player1point == Player2point && Player1point < 4)
+            if (Player1.Point == Player2.Point && Player1.Point < 4)
             {
-                if (Player1point == 0)
+                if (Player1.Point == 0)
                     score = "Love";
-                if (Player1point == 1)
+                if (Player1.Point == 1)
                     score = "Fifteen";
-                if (Player1point == 2)
+                if (Player1.Point == 2)
                     score = "Thirty";
                 score += "-All";
             }
 
-            if (Player1point == Player2point && Player1point >= 3)
+            if (Player1.Point == Player2.Point && Player1.Point >= 3)
                 score = "Deuce";
 
-            if (Player1point > 0 && Player2point == 0)
+            if (Player1.Point > 0 && Player2.Point == 0)
             {
-                if (Player1point == 1)
-                    Player1res = "Fifteen";
-                if (Player1point == 2)
-                    Player1res = "Thirty";
-                if (Player1point == 3)
-                    Player1res = "Forty";
+                if (Player1.Point == 1)
+                    Player1.Result = "Fifteen";
+                if (Player1.Point == 2)
+                    Player1.Result = "Thirty";
+                if (Player1.Point == 3)
+                    Player1.Result = "Forty";
 
-                Player2res = "Love";
-                score = Player1res + "-" + Player2res;
+                Player2.Result = "Love";
+                score = Player1.Result + "-" + Player2.Result;
             }
 
-            if (Player2point > 0 && Player1point == 0)
+            if (Player2.Point > 0 && Player1.Point == 0)
             {
-                if (Player2point == 1)
-                    Player2res = "Fifteen";
-                if (Player2point == 2)
-                    Player2res = "Thirty";
-                if (Player2point == 3)
-                    Player2res = "Forty";
+                if (Player2.Point == 1)
+                    Player2.Result = "Fifteen";
+                if (Player2.Point == 2)
+                    Player2.Result = "Thirty";
+                if (Player2.Point == 3)
+                    Player2.Result = "Forty";
 
-                Player1res = "Love";
-                score = Player1res + "-" + Player2res;
+                Player1.Result = "Love";
+                score = Player1.Result + "-" + Player2.Result;
             }
 
-            if (Player1point > Player2point && Player1point < 4)
+            if (Player1.Point > Player2.Point && Player1.Point < 4)
             {
-                if (Player1point == 2)
-                    Player1res = "Thirty";
-                if (Player1point == 3)
-                    Player1res = "Forty";
-                if (Player2point == 1)
-                    Player2res = "Fifteen";
-                if (Player2point == 2)
-                    Player2res = "Thirty";
-                score = Player1res + "-" + Player2res;
+                if (Player1.Point == 2)
+                    Player1.Result = "Thirty";
+                if (Player1.Point == 3)
+                    Player1.Result = "Forty";
+                if (Player2.Point == 1)
+                    Player2.Result = "Fifteen";
+                if (Player2.Point == 2)
+                    Player2.Result = "Thirty";
+                score = Player1.Result + "-" + Player2.Result;
             }
 
-            if (Player2point > Player1point && Player2point < 4)
+            if (Player2.Point > Player1.Point && Player2.Point < 4)
             {
-                if (Player2point == 2)
-                    Player2res = "Thirty";
-                if (Player2point == 3)
-                    Player2res = "Forty";
-                if (Player1point == 1)
-                    Player1res = "Fifteen";
-                if (Player1point == 2)
-                    Player1res = "Thirty";
-                score = Player1res + "-" + Player2res;
+                if (Player2.Point == 2)
+                    Player2.Result = "Thirty";
+                if (Player2.Point == 3)
+                    Player2.Result = "Forty";
+                if (Player1.Point == 1)
+                    Player1.Result = "Fifteen";
+                if (Player1.Point == 2)
+                    Player1.Result = "Thirty";
+                score = Player1.Result + "-" + Player2.Result;
             }
 
-            if (Player1point > Player2point && Player2point >= 3)
+            if (Player1.Point > Player2.Point && Player2.Point >= 3)
             {
                 score = "Advantage player1";
             }
 
-            if (Player2point > Player1point && Player1point >= 3)
+            if (Player2.Point > Player1.Point && Player1.Point >= 3)
             {
                 score = "Advantage player2";
             }
 
-            if (Player1point >= 4 && Player2point >= 0 && (Player1point - Player2point) >= 2)
+            if (Player1.Point >= 4 && Player2.Point >= 0 && (Player1.Point - Player2.Point) >= 2)
             {
                 score = "Win for player1";
             }
 
-            if (Player2point >= 4 && Player1point >= 0 && (Player2point - Player1point) >= 2)
+            if (Player2.Point >= 4 && Player1.Point >= 0 && (Player2.Point - Player1.Point) >= 2)
             {
                 score = "Win for player2";
             }
@@ -110,12 +105,12 @@ namespace RefactortingTennisGame2
 
         public void P1Score()
         {
-            Player1point++;
+            Player1.Point++;
         }
 
         public void P2Score()
         {
-            Player2point++;
+            Player2.Point++;
         }
 
         public void WonPoint(string player)
